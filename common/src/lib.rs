b4badbe147b5f4fb1dac4df8fc60e946457eba90
Dzuchun <Infernodambik@gmail.com>
2025-02-06 19:48:42 +0200

    Register & Login body structs

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,20 @@
-#![no_std]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RegisterRequest {
+    pub email: String,
+    pub name: String,
+    pub pass: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RegisterResponse {/* probably the same as `LoginResponse`? */}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LoginRequest {
+    pub name: String,
+    pub pass: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LoginResponse {/* some auth-providing fields */}
