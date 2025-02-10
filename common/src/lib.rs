c1c799dd3f6fee229612589e0a12fbb80c193ce6
Dzuchun <Infernodambik@gmail.com>
2025-02-10 17:34:26 +0200

    Some primitives related to quests

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,4 @@
-use derive_more::{From, Into};
+use derive_more::{Display, From, FromStr, Into};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -58,3 +58,70 @@ pub struct SessionKey(Uuid);
 // (and convert for .webp?)
 #[derive(Debug, Clone, From)]
 pub struct Avatar(pub Vec<u8>);
+
+#[derive(
+    Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash, From, FromStr, Display,
+)]
+#[display("{_0}")]
+pub struct QuestId(pub Uuid);
+
+#[derive(Debug, Clone, Serialize, Deserialize, From)]
+pub struct Timestamp(pub chrono::NaiveDateTime);
+
+#[derive(Debug, Clone, Serialize, Deserialize, From)]
+pub struct Completion {
+    pub completed: u32,
+    pub total_pages: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct QuestHistoryRecord {
+    pub user_id: UserId,
+    pub quest_id: QuestId,
+    pub started_at: Timestamp,
+    pub finished_at: Timestamp,
+    pub completion: Completion,
+}
+
+pub const QUEST_HISTORY_PAGE_SIZE: usize = 20;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct QuestHistoryPage {
+    /// max len is defined in the constant above
+    pub data: Box<[QuestHistoryRecord]>,
+    pub page: u32,
+    pub total_pages: u32,
+}
+
+// TODO: multiple moderators
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, TryFrom)]
+#[try_from(repr)]
+#[repr(u8)]
+pub enum QuestState {
+    Dummy = 255,
+}
+
+impl From<QuestState> for u8 {
+    fn from(value: QuestState) -> Self {
+        value as u8
+    }
+}
+
+/// A quest info required to display a quest on user's home/profile
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserOwnedQuestRecord {
+    pub id: QuestId,
+    pub user_id: UserId,
+    pub state: QuestState,
+    /* proly more fields? */
+}
+
+pub const USER_OWNED_QUESTS_PAGE_SIZE: usize = 20;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserOwnedQuestsPage {
+    /// max len is limited in the constant above
+    pub data: Box<[UserOwnedQuestRecord]>,
+    pub page: u32,
+    pub total_pages: u32,
+}
